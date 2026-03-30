96de0fd3a229cc8835589d6d47fc9a18d6f72ad7
Daniel Jimenez Graindorge <danjimen@c2r1s4.42madrid.com>
2026-03-30 09:05:39 +0200

    Removed unnecesary tabs

--- django/0_Oob/ex01/intern.py
+++ django/0_Oob/ex01/intern.py
@@ -4,17 +4,17 @@ class Intern:
 
     def __str__(self):
         return self.Name
-    
+
     class Coffee:
         def __str__(self):
             return ("This is the worst coffee you ever tasted.")
 
     def work(self):
         raise Exception("I’m just an intern, I can’t do that...")
-    
+
     def make_coffee(self):
         return self.Coffee()
-    
+
 if __name__ == "__main__":
     unnamed = Intern()
     print(unnamed)
